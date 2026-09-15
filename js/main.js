c021f83a28cff18db94df4ea7eccfc347bdb8561
venom-cooker <bavruksergij@gmail.com>
2026-09-15 06:48:17 +0000

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-$(function(){
+$(function () {
+let top = $(".article");
+let topH = top.height();
+let header = $(".header");
+let scrollPos = $(window).scrollTop();
 
+$(window).on("scroll", function () {
+scrollPos = $(window).scrollTop();
+if (scrollPos > topH) {
+header.addClass("fixed");
+} else {
+header.removeClass("fixed");
+}
+});
 });
