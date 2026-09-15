2bb8c03f10ab2968867d6105fca2073205537ff9
venom-cooker <bavruksergij@gmail.com>
2026-09-15 08:00:05 +0000

    roma

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 $(function () {
-  
+  const $header = $(".header");
+  const $top = $(".article");
+  let topH = $top.outerHeight();
+  let ticking = false;
+
+  function updateHeader() {
+    const scrollPos = window.scrollY;
+    if (scrollPos > topH) {
+      $header.addClass("fixed");
+    } else {
+      $header.removeClass("fixed");
+    }
+    ticking = false;
+  }
+
+  $(window).on("scroll", function () {
+    if (!ticking) {
+      requestAnimationFrame(updateHeader);
+      ticking = true;
+    }
+  });
+
+  $(window).on("resize", function () {
+    topH = $top.outerHeight();
+  });
 });
