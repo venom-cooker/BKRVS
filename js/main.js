1a0ddec7c814156fa6e11ef5857136b06669ba07
venom-cooker <bavruksergij@gmail.com>
2026-09-15 07:31:37 +0000

    djgslgu

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
 $(function () {
-  const $header = $(".header");
-  const $top = $(".article");
-  let topH = $top.outerHeight();
-  let ticking = false;
-
-  function updateHeader() {
-    const scrollPos = window.scrollY;
-    if (scrollPos > topH) {
-      $header.addClass("fixed");
-    } else {
-      $header.removeClass("fixed");
-    }
-    ticking = false;
-  }
-
-  $(window).on("scroll", function () {
-    if (!ticking) {
-      requestAnimationFrame(updateHeader);
-      ticking = true;
-    }
-  });
-
-  // перераховуємо висоту блоку при зміні розміру вікна
-  $(window).on("resize", function () {
-    topH = $top.outerHeight();
-  });
+  
 });
